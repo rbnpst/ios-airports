3c4ec74572e2f62a62eef99f01d2b190ae61bce9
Robin Post <RobinPost.dev@gmail.com>
2017-10-16 23:04:02 +0200

    Initial Commit

---
+++ ios-airports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-airports
+//
+//  Created by Robin Post on 16/10/2017.
+//  Copyright © 2017 Robin Post. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
